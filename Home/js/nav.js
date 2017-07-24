a62f2be2931945c5fcf112d971923bf6b1586796
zengfuqin <331875159@qq.com>
2017-07-24 16:33:47 +0800

    2017724_fruitday

---
+++ Home/js/nav.js
@@ -0,0 +1,356 @@
+
+		*{
+			margin:0;
+			padding: 0;
+		}
+		header{
+			width: 100%;
+			height: 150px;
+			// background: yellow;
+		}
+		header #info_box{
+			width: 100%;
+			height: 30px;
+			background: #eee;
+		}
+		header #info_box #info{
+			/*background: #eee;*/
+			width: 1287px;
+			height: 30px;
+			margin: 0 auto;
+		}
+		header #info_box #info #info_left{
+			/*background: #e2e;*/
+			width: 232px;
+			height: 30px;
+			float: left;
+		}
+		header #info_box #info #info_left #address{
+			float: left;
+			color: #999;
+			font-size: 12px;
+		}
+		header #info_box #info #info_left #address a{
+			display: block;
+			width: 65px;
+			height: 30px;
+			line-height: 30px;
+			padding-left: 10px;
+			/*background: yellow;*/
+			color: #999;
+			font-size: 12px;
+			text-decoration: none;
+			float: left;
+		}
+		header #info_box #info #info_left #address a #add_car{
+			padding-top: 10px;
+			float: left;
+		}
+		header #info_box #info #info_left #address a #add_car2{
+			display: none;
+		}
+		header #info_box #info #info_left #address a:hover #add_car{
+			display: none;
+		}
+		header #info_box #info #info_left #address a:hover #add_car2{
+			display: block;
+			float: left;
+			padding-top: 10px;
+		}
+		header #info_box #info #info_left #address a #add_arr{
+			float:right;
+			padding-right: 5px;
+			padding-top: 10px;
+		}
+		header #info_box #info #info_left #address a #add_arr2{
+			display: none;
+		}
+		header #info_box #info #info_left #address a:hover #add_arr{
+			display: none;
+		}
+		header #info_box #info #info_left #address a:hover #add_arr2{
+			display: block;
+			float: right;
+			padding-right: 5px;
+			padding-top: 10px;
+		}
+		header #info_box #info #info_left #address a:hover{
+			background: #fff;
+			line-height: 30px;
+			height: 30px;
+			display: block;
+		}
+		header #info_box #info #info_left #address a span{
+			/*background: purple;*/
+			display: block;
+			width: 30px;
+			line-height: 30px;
+			margin-left: 4px;
+			float: left;
+		}
+		header #info_box #info #info_left #address a:hover span{
+			/*background: red;*/
+			display: block;
+			width: 30px;
+			line-height: 30px;
+			float: left;
+		}
+		header #info_box #info #info_left a #address_list{
+			width: 280px;
+			height: 380px;
+			background: #0f0;
+			position: relative;
+			top:30px;
+			right: 10px;
+			display: none;
+		}
+		header #info_box #info #info_left a:hover #address_list{
+			display: block;
+		}
+		header #info_box #info #info_left #add_info{
+			float: left;
+			color: #999;
+			font-size: 12px;
+			line-height: 30px;
+			padding-left: 3px;
+		}
+		header #info_box #info #info_left #xingyeda a{
+			color: #999;
+			font-size: 12px;
+			text-decoration: none;
+			line-height: 30px;
+			display: block;
+			height: 30px;
+			color: #64a131;
+		}
+		header #info_box #info #info_left #xingyeda a:hover{
+			text-decoration: underline;
+		}
+		header #info_box #info #info_left #xingyeda{
+			float: left;
+			color: #999;
+			font-size: 12px;
+		}
+		header #info_box #info #info_right{
+			/*background: #e2e;*/
+			width: 550px;
+			height: 30px;
+			float: right;
+		}
+		header #info_box #info #info_right ul{
+			list-style:none;
+		}
+		header #info_box #info #info_right ul li{
+			float: left;
+			width: 90px;
+			height: 30px;
+			color: #999;
+			font-size:12px;
+			line-height: 30px;
+			/*background: #0f0;*/
+		}
+		header #info_box #info #info_right ul  #person{
+			width: 100px;
+		}
+		header #info_box #info #info_right ul li a{
+			text-decoration: none;
+			color: #999;
+			font-size:12px;
+			display: block;
+			width: 70px;
+			height: 28px;
+			/*background: yellow;*/
+			text-align: center;
+			float: left;
+			padding-top: 3px;
+			margin-right: 5px;
+		}
+		header #info_box #info #info_right ul #register_li{
+			/*background: yellow;*/
+			width: 60px;
+		}
+		header #info_box #info #info_right ul #register{
+			width: 40px;
+			/*background: red;*/
+		}
+		header #info_box #info #info_right ul li a #phone_png{
+			margin-right: 7px;
+		}
+		header #info_box #info #info_right ul li  #person_png{
+			margin-right: 7px;
+
+		}
+		header #info_box #info #info_right ul li a:hover{
+			color: #f6aa00;
+		}
+		header #info_box #info #info_right ul li  #ad_a:hover{
+			background: #fff;
+		}
+		header #info_box #info #info_right ul li  #ad_a #ad_a_box{
+			width: 240px;
+			height: 158px;
+			background: #00f;
+			position: relative;
+			right: 170px;
+			display: none;
+		}
+		header #info_box #info #info_right ul li  #ad_a:hover #ad_a_box{
+			display: block;
+		}
+		header #info_box #info #info_right ul li  #phone_a:hover{
+			background: #fff;
+		}
+		header #info_box #info #info_right ul li  #phone_a #phone_a_box{
+			width: 280px;
+			height: 320px;
+			background: #0ff;
+			position: relative;
+			right: 210px;
+			display: none;
+		}
+		header #info_box #info #info_right ul li  #phone_a:hover #phone_a_box{
+			display: block;
+		}
+		header #logo_box_box{
+			width: 100%;
+			height: 120px;
+		}
+		header #logo_box{
+			width: 1287px;
+			height: 120px;
+			/*background: #ff5;*/
+			margin:0 auto;
+		}
+		header #logo_box #logo{
+			width: 302px;
+			height: 120px;
+			/*background: purple;*/
+			float: left;
+		}
+		header #logo_box #logo a{
+			display: block;
+			background: url(./images/logo.png) no-repeat 10px center;
+			width: 296px;
+			height: 74px;
+			margin-top: 23px;
+		}
+		header #logo_box nav{
+			width: 482px;
+			height: 80px;
+			/*background: #ccc;*/
+			margin-left: 42px;
+			float: left;
+			margin-top: 20px;
+		}
+		header #logo_box nav ul{
+			list-style-type: none;
+
+		}
+		header #logo_box nav ul li{
+			float: left;
+			font-size: 14px;
+			width: 90px;
+			padding-top: 20px;
+			padding-bottom: 40px;
+			/*background: red;*/
+		}
+		header #logo_box nav ul  #index_box{
+			border-bottom: 1px solid #0f0;
+		}
+		header #logo_box nav ul li a{
+			display: block;
+			width: 90px;
+			height: 40px;
+			text-decoration: none;
+			color: #999;
+			/*background: blue;*/
+			text-align: center;
+			border-right: 1px solid #eee;
+		}
+		header #logo_box nav ul li a:hover{
+			background: #eee;
+		}
+		header #logo_box nav ul li #index{
+			color: #64a131;
+		}
+		header #logo_box nav ul li #vip{
+			border-right: none;
+		}
+		header #logo_box nav ul li a span{
+			display: block;
+			padding-top: 2px;
+		}
+		header #logo_box #search_box{
+			width: 256px;
+			height: 120px;
+			/*background: purple;*/
+			float: right;
+		}
+		header #logo_box #search_box #search{
+			width: 160px;
+			height: 50px;
+			/*background: #ccc;*/
+			padding-top: 30px;
+			float: left;
+		}
+		header #logo_box #search_box #search #search_input{
+			border: none;
+			border-bottom: 1px solid #000;
+			padding-bottom: 5px;
+			font-size: 12px;
+			width: 130px;
+			height: 22px;
+			float: left;
+		}
+		header #logo_box #search_box #search  #search_png{
+			float: left;
+		}
+		header #logo_box #search_box #search  #search_png img{
+			border-bottom: 1px solid #000;
+		}
+		header #logo_box #search_box #search ul{
+			list-style:none;
+			color: #999;
+			font-size: 12px;
+			padding-top: 5px;
+		}
+		header #logo_box #search_box #search ul li{
+			float: left;
+		}
+		header #logo_box #search_box #search ul li a{
+			color: #999;
+			text-decoration:none;
+			display: block;
+			/*width: 29px;
+			width: 22px;*/
+			padding-right: 5px; 
+			font-size: 12px;
+		}
+		header #logo_box #search_box #search ul li a:hover{
+			text-decoration: underline;
+		}
+		header #logo_box #search_box #gouwulan{
+			/*background: #f00;*/
+			width: 38px;
+			height: 41px;
+			float: left;
+			margin-left: 30px;
+			margin-top: 30px;
+		}
+		#scroll_box{
+			width: 1287px;
+			height: 400px;
+			background: red;
+			margin:0 auto;
+		}
+		#content_box{
+			width: 1287px;
+			/*overflow: hidden;*/
+			height: 1000px;
+			background: blue;
+			margin: 0 auto;
+		}
+		footer{
+			
+		}
+		</style>
